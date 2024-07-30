348d3e75383b5df2924c2b536af9da136e35c068
Anonymous <anonymous@anonymous>
2024-07-29 21:11:12 -0700

    commit

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -3,33 +3,31 @@ import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
 const FeatureList = [
+
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Easy to Integrate',
+    Svg: require('@site/static/img/svg1.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Support multiple programming languages, support most CI/CD platforms.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
+      title: 'Easy to Use',
+      Svg: require('@site/static/img/svg2.svg').default,
+      description: (
+        <>
+          Apply Shift-Left strategy for your organization with Codety code analysis integration.
+        </>
+      ),
+    },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Secure',
+    Svg: require('@site/static/img/svg3.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Minimal permission required by respecting the Principle of Least Privilege(POLP).
       </>
     ),
   },
